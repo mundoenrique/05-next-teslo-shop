ae82507343242c4bd992a678904b3b41dc644272
mundoenrique <mundoenrique@hotmail.com>
2022-07-26 10:12:58 -0400

    Pagina inicial temporal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+
 import { ShopLayout } from '../components/layuots';
+import { initialData } from '../database/products';
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +13,18 @@ const Home: NextPage = () => {
       <Typography variant="h2" sx={{ mb: 1 }}>
         Todos los productos
       </Typography>
+
+      <Grid container spacing={4}>
+        {initialData.products.map((product) => (
+          <Grid item xs={6} sm={4} key={product.slug}>
+            <Card>
+              <CardActionArea>
+                <CardMedia component="img" image={`products/${product.images[0]}`} alt={product.title} />
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </ShopLayout>
   );
 };
