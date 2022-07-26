ea146258a8f4bd11ad6dadc5a6cea17858433b34
mundoenrique <mundoenrique@hotmail.com>
2022-07-26 15:50:15 -0400

    Selector de tallas

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Chip, Grid, Typography } from '@mui/material';
 
 import { ShopLayout } from '../../components/layuots/ShopLayout';
-import { ProductSlideshow } from '../../components/products';
+import { ProductSlideshow, SizeSelector } from '../../components/products';
 import { ItemCounter } from '../../components/ui';
 import { initialData } from '../../database/products';
 
@@ -27,6 +27,7 @@ const ProductPage = () => {
             <Box sx={{ my: 2 }}>
               <Typography variant="subtitle2">Cantidad</Typography>
               <ItemCounter />
+              <SizeSelector /* selectedSize={product.sizes[0]} */ sizes={product.sizes} />
             </Box>
             {/* Agregar al carrito */}
             <Button color="secondary" className="circular-btn">
