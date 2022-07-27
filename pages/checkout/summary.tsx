20889894ee6bfc9f7ddb81e992f8087df6c39c07
mundoenrique <mundoenrique@hotmail.com>
2022-07-27 13:50:26 -0400

    Resumen de la compra

--- pages/checkout/summary.tsx
+++ pages/checkout/summary.tsx
@@ -1,27 +1,45 @@
-import { Box, Button, Card, CardContent, Divider, Grid, Typography } from '@mui/material';
+import NextLink from 'next/link';
+import { Box, Button, Card, CardContent, Divider, Grid, Link, Typography } from '@mui/material';
 
 import { ShopLayout } from '../../components/layuots/ShopLayout';
 import { CartList, OrderSummary } from '../../components/cart';
 
 const SummaryPage = () => {
   return (
-    <ShopLayout title={'Carrito - 3'} pageDescription="Carrito de compras de la tienda">
+    <ShopLayout title="Resumen de orden" pageDescription="Resumen de la orden">
       <Typography variant="h1" component="h1">
-        Carrito
+        Resumen de la orden
       </Typography>
       <Grid container>
         <Grid item xs={12} sm={7}>
-          <CartList editable />
+          <CartList />
         </Grid>
         <Grid item xs={12} sm={5}>
           <Card className="summary-card">
             <CardContent>
-              <Typography variant="h2">Orden</Typography>
+              <Typography variant="h2">Resumen (3 productos)</Typography>
               <Divider sx={{ my: 1 }} />
+              <Box display="flex" justifyContent="space-between">
+                <Typography variant="subtitle1">Dirección de entrega</Typography>
+                <NextLink href="/checkout/address" passHref>
+                  <Link underline="always">Editar</Link>
+                </NextLink>
+              </Box>
+              <Typography>Enrique Peñaloza</Typography>
+              <Typography>323 Algún lugar</Typography>
+              <Typography>Sitttsville, HYA 23S</Typography>
+              <Typography>Venezuela</Typography>
+              <Typography>+58 5502100</Typography>
+              <Divider sx={{ my: 1 }} />
+              <Box display="flex" justifyContent="end">
+                <NextLink href="/cart" passHref>
+                  <Link underline="always">Editar</Link>
+                </NextLink>
+              </Box>
               <OrderSummary />
               <Box sx={{ mt: 3 }}>
                 <Button color="secondary" className="circular-btn" fullWidth>
-                  Checkout
+                  Confirmar Orden
                 </Button>
               </Box>
             </CardContent>
