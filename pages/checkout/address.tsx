a003398a3bb9199d8860880673ac94e5ae644ef9
mundoenrique <mundoenrique@hotmail.com>
2022-08-02 15:05:14 -0400

    Valida token para ir a la pagina de dirección

--- pages/checkout/address.tsx
+++ pages/checkout/address.tsx
@@ -1,5 +1,8 @@
+import { GetServerSideProps } from 'next';
 import { Box, Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
+
 import { ShopLayout } from '../../components/layouts';
+import { jwt } from '../../utils';
 
 const AddressPage = () => {
   return (
@@ -52,6 +55,34 @@ const AddressPage = () => {
       </Box>
     </ShopLayout>
   );
+};
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { token = '' } = req.cookies;
+
+  let isValidToken = false;
+
+  try {
+    await jwt.isValidToken(token);
+    isValidToken = true;
+  } catch (error) {
+    isValidToken = false;
+  }
+
+  if (!isValidToken) {
+    return {
+      redirect: {
+        destination: '/auth/login?p=/checkout/address',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default AddressPage;
