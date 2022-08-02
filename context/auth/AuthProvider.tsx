c2b69c86e6aea921a531f972b522a96b37872510
mundoenrique <mundoenrique@hotmail.com>
2022-08-01 23:59:30 -0400

    Auth provider

---
+++ context/auth/AuthProvider.tsx
@@ -0,0 +1,31 @@
+import { FC, useReducer } from 'react';
+import { IUser } from '../../interfaces';
+import { AuthContext, authReducer } from './';
+
+interface Props {
+  children: JSX.Element;
+}
+
+export interface AuthState {
+  isLoggedIn: boolean;
+  user?: IUser;
+}
+
+const AUTH_INITIAL_STATE: AuthState = {
+  isLoggedIn: false,
+  user: undefined,
+};
+
+export const AuthProvider: FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, AUTH_INITIAL_STATE);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...state,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
