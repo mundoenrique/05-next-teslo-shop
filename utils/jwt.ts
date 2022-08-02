d02352143e8c651b5bbc62e23cbfea7ca6e311ff
mundoenrique <mundoenrique@hotmail.com>
2022-08-01 21:09:24 -0400

    Valida token

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import { resolve } from 'path';
 
 export const signToken = (_id: string, email: string) => {
   if (!process.env.JWT_SECRET_SEED) {
@@ -15,4 +16,24 @@ export const signToken = (_id: string, email: string) => {
     // Opciones
     { expiresIn: '30d' }
   );
+};
+
+export const isValidToken = (token: string): Promise<string> => {
+  if (!process.env.JWT_SECRET_SEED) {
+    throw new Error('No hay semilla de JWT - Revisar variables de entorno');
+  }
+
+  return new Promise((resolve, reject) => {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET_SEED || '', (err, payload) => {
+        if (err) return reject('JWT no es válido');
+
+        const { _id } = payload as { _id: string };
+
+        resolve(_id);
+      });
+    } catch (error) {
+      reject('JWT no es válido');
+    }
+  });
 };
