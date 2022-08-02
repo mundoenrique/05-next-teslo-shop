4af86cab48b609c49b4e20832174cc0c0c2f6f84
mundoenrique <mundoenrique@hotmail.com>
2022-08-02 18:37:43 -0400

    Valida token en middleware

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import * as jose from 'jose';
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname.startsWith('/checkout')) {
+    const token = req.cookies.get('token');
+
+    try {
+      await jose.jwtVerify(token || '', new TextEncoder().encode(process.env.JWT_SECRET_SEED || ''));
+      //If no error is thrown, the JWT is valid, you can even the payload if necessary
+      return NextResponse.next();
+    } catch (error) {
+      console.error(`JWT Invalid or not signed in`, { error });
+      const { protocol, host, pathname } = req.nextUrl;
+
+      return NextResponse.redirect(`${protocol}//${host}/auth/login?p=${pathname}`);
+    }
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/checkout/:path*',
+};
